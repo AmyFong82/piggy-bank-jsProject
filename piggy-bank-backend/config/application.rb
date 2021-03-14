f30e71620633f114b60e53ee47da1e7d54ec07eb
AmyFong82 <amyhfong@gmail.com>
2021-03-14 01:01:28 -0500

    adding 2 lines about cookies in Application.rb in locales.

--- piggy-bank-backend/config/application.rb
+++ piggy-bank-backend/config/application.rb
@@ -36,5 +36,9 @@ module PiggyBankBackend
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # 2 lines from https://pragmaticstudio.com/tutorials/rails-session-cookies-for-api-authentication tutorial
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore
   end
 end
